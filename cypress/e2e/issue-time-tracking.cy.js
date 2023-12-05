6c766d19f81bc54e5f64aefdfd3b2983fe996158
kkomaenga <a.aleksandrova1998@gmail.com>
2023-12-05 19:22:36 +0200

    Time tracking v 05.12

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,126 +1,139 @@
-describe('Issue comments creating, editing and deleting', () => {
+describe('Time-tracking functionality', () => {
     beforeEach(() => {
-        cy.visit('/');
-        cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-            cy.visit(url + '/board');
-            cy.contains('This is an issue of type: Task.').click();
+        navigateToIssueDetailsModal()
         });
-    });
 
     const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
+    const Estimation = () => cy.get('[placeholder="Number"]');
+    const EstimatedTime = () => cy.get('.sc-rBLzX.irwmBe div:last-child');
+    const TimeLoggingButton = () => cy.get('.sc-bMvGRv.IstSR');
+    const TrackingModal = () => cy.get('[data-testid="modal:tracking"]');
+    const LoggedTime = () => cy.get('.sc-rBLzX.irwmBe div:first-child');
 
-    it('Should create a comment successfully', () => {
-        const comment = 'TEST_COMMENT';
-
-        getIssueDetailsModal().within(() => {
-            cy.contains('Add a comment...')
-                .click();
-
-            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
-
-            cy.contains('button', 'Save')
-                .click()
-                .should('not.exist');
-
-            cy.contains('Add a comment...').should('exist');
-            cy.get('[data-testid="issue-comment"]').should('contain', comment);
-        });
-    });
-
-    it('Should edit a comment successfully', () => {
-        const previousComment = 'An old silent pond...';
-        const comment = 'TEST_COMMENT_EDITED';
+    it('Test case 1: Time Estimation Functionality', () => {
 
         getIssueDetailsModal().within(() => {
-            cy.get('[data-testid="issue-comment"]')
-                .first()
-                .contains('Edit')
+            Estimation()
                 .click()
-                .should('not.exist');
-
-            cy.get('textarea[placeholder="Add a comment..."]')
-                .should('contain', previousComment)
                 .clear()
-                .type(comment);
-
-            cy.contains('button', 'Save')
+                .type('8')
+                .should('be.visible') 
+                .should('have.value', '8');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '8h estimated');
+            Estimation()
                 .click()
-                .should('not.exist');
-
-            cy.get('[data-testid="issue-comment"]')
-                .should('contain', 'Edit')
-                .and('contain', comment);
+                .clear()
+                .should('have.value', '');
+            EstimatedTime().should('not.contain', '8h estimated');
+            Estimation()
+                .click()
+                .type('10')
+                .should('be.visible') 
+                .should('have.value', '10');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '10h estimated');
         });
     });
 
-    it('Should delete a comment successfully', () => {
-        getIssueDetailsModal()
-            .find('[data-testid="issue-comment"]')
-            .contains('Delete')
-            .click();
-
-        cy.get('[data-testid="modal:confirm"]')
-            .contains('button', 'Delete comment')
-            .click()
-            .should('not.exist');
-
-        getIssueDetailsModal()
-            .find('[data-testid="issue-comment"]')
-            .should('not.exist');
-    });
-
-    it('Test case 1: Test combination, Comments functionality', () => {
-        const comment = 'this is comment for test case 1, assignment 1';
-        const EditedComment = 'edited comment for assignment 1';
+    it('Test case 2: Time Logging Functionality', () => {
 
         getIssueDetailsModal().within(() => {
-            cy.contains('Add a comment...')
-                .click();
-
-            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
-
-            cy.contains('button', 'Save')
+            Estimation()
                 .click()
-                .should('not.exist');
-
-            cy.contains('Add a comment...').should('exist');
-            cy.get('[data-testid="issue-comment"]').should('contain', comment);
-
-            cy.get('[data-testid="issue-comment"]')
-                .first()
-                .contains('Edit')
-                .click()
-                .should('not.exist');
-
-            cy.get('textarea[placeholder="Add a comment..."]')
-                .should('contain', comment)
                 .clear()
-                .type(EditedComment);
-
-            cy.contains('button', 'Save')
+                .type('8')
+                .should('be.visible') 
+                .should('have.value', '8');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '8h estimated');
+            getTimeTrackingModal()
+        });
+        checkTrackingModalVisibility();
+        TrackingModal().within(()=> {
+            Estimation()
+                .first()
+                .click()
+                .clear()
+                .type('8');
+            cy.contains('button', 'Done')
                 .click()
                 .should('not.exist');
+        });
 
-            cy.get('[data-testid="issue-comment"]')
-                .should('contain', 'Edit')
-                .and('contain', EditedComment);
-                
-                cy.reload();
-
-            cy.get('[data-testid="issue-comment"]')
-                .and('contain', EditedComment)
-                .contains('Delete')
-                .click();
-            });
-
-        cy.get('[data-testid="modal:confirm"]').within(() => {
-            cy.contains("Are you sure you want to delete this comment?Once you delete, it's gone for good.")
-            .should('be.visible');
-            cy.contains('button', 'Delete comment').first().click().should('not.exist');
-            });
-            getIssueDetailsModal().within(() => {
-                cy.get('[data-testid="issue-comment"]').should('have.length', 1); 
-                cy.get('[data-testid="issue-comment"]').first().contains(EditedComment).should('not.exist');
-            });
+        getIssueDetailsModal().within(() => {
+            LoggedTime()
+                .should('be.visible')
+                .should('have.text', '8h logged');
+            getTimeTrackingModal()
+        });
+        checkTrackingModalVisibility();
+        TrackingModal().within(()=> {
+            Estimation()
+                .first()
+                .click()
+                .clear()
+                .should('have.value', '');
+            cy.contains('button', 'Done')
+                .click()
+                .should('not.exist');
+        });
+        getIssueDetailsModal().within(() => {
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '8h estimated');
+            LoggedTime()
+                .should('be.visible')
+                .should('have.text', 'No time logged');
+            Estimation()
+                .click()
+                .clear()
+                .type('10')
+                .should('be.visible') 
+                .should('have.value', '10');
+            getTimeTrackingModal()
+        });
+        checkTrackingModalVisibility();
+        TrackingModal().within(()=> {
+            Estimation()
+                .first()
+                .click()
+                .type('9');
+            //adding Remaining time
+            Estimation()
+                .eq(1).click()
+                .type('1');
+            cy.contains('button', 'Done')
+                .click()
+                .should('not.exist');
+        });
+        getIssueDetailsModal().within(() => {
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '1h remaining');
+            LoggedTime()
+                .should('be.visible')
+                .should('have.text', '9h logged');
         });
     });
+
+        function navigateToIssueDetailsModal(){
+            cy.visit('/');
+            cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+                cy.visit(url + '/board');
+                cy.contains('This is an issue of type: Task.').click();
+                getIssueDetailsModal().should('be.visible');
+            }); 
+        };
+
+        function getTimeTrackingModal(){
+            TimeLoggingButton().click();
+        };
+
+        function checkTrackingModalVisibility(){
+            TrackingModal().should('be.visible');
+        }
+});
