6a87494f441a99e8eb0c4bedeb918b16429e7bed
kkomaenga <a.aleksandrova1998@gmail.com>
2023-12-09 03:23:05 +0200

    v 09.12

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -9,6 +9,9 @@ describe('Time-tracking functionality', () => {
     const TimeLoggingButton = () => cy.get('[data-testid="icon:stopwatch"]');
     const TrackingModal = () => cy.get('[data-testid="modal:tracking"]');
     const LoggedTime = () => cy.get('.sc-rBLzX.irwmBe div:first-child');
+    const CloseIssueDetailModal = () => cy.get('[data-testid="icon:close"]').first().click();
+    const CheckNumberPlaceholderVisibility = () => cy.get('input[placeholder="Number"]').should('be.visible');
+
 
     it('Test case 1: Time Estimation Functionality', () => {
 
@@ -117,6 +120,134 @@ describe('Time-tracking functionality', () => {
             LoggedTime()
                 .should('be.visible')
                 .should('have.text', '9h logged');
+        });
+    });
+
+    it('Test case: Add estimation; Update estimation; Remove estimation', () => {
+
+        getIssueDetailsModal().within(() => {
+            Estimation()
+                .click()
+                .clear()
+                .should('be.visible')
+                .should('have.value', '');
+            EstimatedTime().should('not.contain', '8h estimated');
+            Estimation()
+                .click()
+                .type('10')
+                .should('be.visible') 
+                .should('have.value', '10');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '10h estimated');
+            CloseIssueDetailModal();
+        });
+       
+        cy.contains('This is an issue of type: Task.').click();
+        getIssueDetailsModal().within(() => {
+            Estimation()
+                .should('be.visible') 
+                .should('have.value', '10');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '10h estimated');
+            Estimation()
+                .click()
+                .clear()
+                .type('20');
+            CloseIssueDetailModal();
+        });
+        cy.contains('This is an issue of type: Task.').click();
+        getIssueDetailsModal().within(() => {
+            Estimation()
+                .should('be.visible') 
+                .should('have.value', '20');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '20h estimated');
+            Estimation()
+                .click()
+                .clear();
+            CloseIssueDetailModal();
+        });
+        cy.contains('This is an issue of type: Task.').click();
+        getIssueDetailsModal().within(() => {
+            Estimation()
+                .click()
+                .clear()
+                .should('have.value', '');
+            EstimatedTime().should('not.contain', '8h estimated');
+            CheckNumberPlaceholderVisibility();
+            CloseIssueDetailModal();
+        });
+    });
+
+    it('Test case: Log Time; Remove logged time', () => {
+
+        getIssueDetailsModal().within(() => {
+            Estimation()
+                .click()
+                .clear()
+                .should('be.visible')
+                .should('have.value', '');
+            EstimatedTime().should('not.contain', '8h estimated');
+            Estimation()
+                .click()
+                .type('10')
+                .should('be.visible') 
+                .should('have.value', '10');
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '10h estimated');
+            getTimeTrackingModal()
+        });
+        checkTrackingModalVisibility();
+        TrackingModal().within(()=> {
+            Estimation()
+                .first()
+                .click()
+                .clear()
+                .type('2');
+            //adding Remaining Time
+            Estimation()
+                .eq(1).click()
+                .type('5');
+            cy.contains('button', 'Done')
+                .click()
+                .should('not.exist');
+        });
+        getIssueDetailsModal().within(() => {
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '5h remaining');
+            LoggedTime()
+                .should('be.visible')
+                .should('have.text', '2h logged')
+                .should('not.contain', 'No time logged');
+            getTimeTrackingModal()
+        });
+        checkTrackingModalVisibility();
+        TrackingModal().within(()=> {
+            Estimation()
+                .first()
+                .click()
+                .clear();
+            //adding Remaining Time
+            Estimation()
+                .eq(1).click()
+                .clear();
+            cy.contains('button', 'Done')
+                .click()
+                .should('not.exist');
+        });
+        getIssueDetailsModal().within(() => {
+            EstimatedTime()
+                .should('be.visible')
+                .should('have.text', '10h estimated');
+            LoggedTime()
+                .should('be.visible')
+                .should('have.text', 'No time logged')
+                .should('not.contain', '2h logged');
         });
     });
 
